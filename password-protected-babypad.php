eefc184c73bb3939f9cfa69f9152cc9154b50abf
ベビーカレンダー 諏訪あけみ <akemi-suwa@baby-calendar.jp>
2019-11-21 17:37:09 +0900

    リビジョン更新

--- password-protected-babypad.php
+++ password-protected-babypad.php
@@ -4,7 +4,7 @@
 Plugin Name: パスワード保護 for ベビーパッド
 Plugin URI:
 Description: password protected を元に、ベビーパッド用にスマホでアクセスした時のみパスワードを求めるように、かつパスワードリストからサイトごとにパスワードを読み込めるようにカスタマイズ。
-Version: 1.0.0
+Version: 1.0.1
 Author: Akemi Suwa
 Author URI:
 License: GPLv2
